f746b3bd4f3de0c314ac6e282692e2458d7c1d04
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-02-08 18:18:50 -0500

    Save the working controller directives

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -17,6 +17,23 @@
         request_tracking();
       });
     })
+    .controller('MessageCtrl', function($scope, $state, $http){
+      $scope.$on('$viewContentLoaded', function(event) {
+        $('[selectize]').selectize({
+          plugins: ['remove_button'],
+          persist: false,
+          create: true,
+          render: {
+            item: function(data, escape) {
+              return '<div>"' + escape(data.text) + '"</div>';
+            }
+          },
+          onDelete: function(values) {
+            return confirm(values.length > 1 ? 'Are you sure you want to remove these ' + values.length + ' items?' : 'Are you sure you want to remove "' + values[0] + '"?');
+          }
+        });
+      });
+    })
     .config(config)
     .run(run)
 

759e9ada412a4ac5d6a9a388efe615a1917c8a09
Lenny Bogdonoff <rememberlenny@gmail.com>
2016-02-09 10:27:34 -0500

    Add mock data

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -29,7 +29,7 @@
             }
           },
           onDelete: function(values) {
-            return confirm(values.length > 1 ? 'Are you sure you want to remove these ' + values.length + ' items?' : 'Are you sure you want to remove "' + values[0] + '"?');
+            console.log('Deleted: ', values)
           }
         });
       });

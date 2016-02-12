47214c6fc493de23f69b5d620bbc86c32a4156ea
rememberlenny <rememberlenny@gmail.com>
2016-02-12 18:41:06 -0500

    Remove build

--- build/assets/js/routes.js
+++
@@ -1,1 +0,0 @@
-var foundationRoutes = [{"name":"settings","url":"/settings","path":"templates/settings.html"},{"name":"reports","url":"/reports","path":"templates/reports.html"},{"name":"report","url":"/report","path":"templates/report.html"},{"name":"inbox","url":"/inbox/:id","path":"templates/message.html"},{"name":"home","url":"/","controller":"DashCtrl","path":"templates/home.html"}]; 
